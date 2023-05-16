cad85126fe3c380ac7feb494e24b053b60ba9e98
Jesse Andrews <jesse@replicate.com>
2023-05-16 01:16:04 +0000

    use controlnet_aux latest improvements

--- predict.py
+++ predict.py
@@ -15,7 +15,8 @@ from diffusers import (
     ControlNetModel,
     StableDiffusionPipeline,
 )
-from controlnet_aux import HEDdetector, OpenposeDetector, MLSDdetector, MidasDetector
+from controlnet_aux import HEDdetector, OpenposeDetector, MLSDdetector, MidasDetector, CannyDetector
+from controlnet_aux.util import ade_palette
 
 
 AUX_IDS = {
@@ -59,6 +60,9 @@ class Predictor(BasePredictor):
                 local_files_only=True,
             ).to("cuda")
 
+        # Canny
+        self.canny = CannyDetector()
+
         # Depth + Normal
         self.midas = MidasDetector.from_pretrained(
             "lllyasviel/ControlNet",
@@ -66,21 +70,21 @@ class Predictor(BasePredictor):
         )
 
         # HED
-        self.controlnet_hed = HEDdetector.from_pretrained("lllyasviel/ControlNet")
+        self.hed = HEDdetector.from_pretrained("lllyasviel/ControlNet", cache_dir=PROCESSORS_CACHE)
 
         # Hough
-        self.mlsd = MLSDdetector.from_pretrained("lllyasviel/ControlNet")
+        self.mlsd = MLSDdetector.from_pretrained("lllyasviel/ControlNet", cache_dir=PROCESSORS_CACHE)
 
         # Seg
         self.controlnet_seg_processor = AutoImageProcessor.from_pretrained(
-            "openmmlab/upernet-convnext-small"
+            "openmmlab/upernet-convnext-small", cache_dir=PROCESSORS_CACHE
         )
         self.controlnet_seg_segmentor = UperNetForSemanticSegmentation.from_pretrained(
-            "openmmlab/upernet-convnext-small"
+            "openmmlab/upernet-convnext-small", cache_dir=PROCESSORS_CACHE
         )
 
         # Pose
-        self.controlnet_pose = OpenposeDetector.from_pretrained("lllyasviel/ControlNet")
+        self.open_pose = OpenposeDetector.from_pretrained("lllyasviel/Annotators", cache_dir=PROCESSORS_CACHE)
 
         print("Setup complete in %f" % (time.time() - st))
 
@@ -195,69 +199,22 @@ class Predictor(BasePredictor):
 
     def process_image(self, image, structure, low_threshold=100, high_threshold=200):
         if structure == 'canny':
-            input_image = self.canny_preprocessor(image, low_threshold, high_threshold)
+            input_image = self.canny(image, low_threshold, high_threshold)
         elif structure == 'depth':
             input_image = self.midas(image)
         elif structure == 'hed':
-            input_image = self.hed_preprocessor(image)
+            input_image = self.hed(image)
         elif structure == 'hough':
-            input_image = self.hough_preprocessor(image)
+            input_image = self.mlsd(image)
         elif structure == 'normal':
-            input_image = self.normal_preprocessor(image)
+            input_image = self.midas(image, depth_and_normal=True)[1]
         elif structure == 'pose':
-            input_image = self.pose_preprocessor(image)
+            input_image = self.open_pose(image)
         elif structure == 'scribble':
-            input_image = self.scribble_preprocessor(image)
+            input_image = self.hed(image, scribble=True)
         elif structure == 'seg':
             input_image = self.seg_preprocessor(image)
         return input_image
-
-    def canny_preprocessor(self, image, low_threshold, high_threshold):
-        # Convert to numpy
-        image = np.array(image)
-        image = cv2.Canny(image, low_threshold, high_threshold)
-        image = image[:, :, None]
-        image = np.concatenate([image, image, image], axis=2)
-        canny_image = Image.fromarray(image)
-        return canny_image
-
-    def hed_preprocessor(self, image):
-        # Convert to numpy
-        image = np.array(image)
-        return self.controlnet_hed(image)
-
-    def hough_preprocessor(self, image):
-        # Convert to numpy
-        image = np.array(image)
-        return self.mlsd(image)
-
-    def normal_preprocessor(self, image):
-        image = self.depth_estimator(image)['predicted_depth'][0]
-        image = image.numpy()
-        image_depth = image.copy()
-        image_depth -= np.min(image_depth)
-        image_depth /= np.max(image_depth)
-        bg_threhold = 0.4
-        x = cv2.Sobel(image, cv2.CV_32F, 1, 0, ksize=3)
-        x[image_depth < bg_threhold] = 0
-        y = cv2.Sobel(image, cv2.CV_32F, 0, 1, ksize=3)
-        y[image_depth < bg_threhold] = 0
-        z = np.ones_like(x) * np.pi * 2.0
-        image = np.stack([x, y, z], axis=2)
-        image /= np.sum(image ** 2.0, axis=2, keepdims=True) ** 0.5
-        image = (image * 127.5 + 127.5).clip(0, 255).astype(np.uint8)
-        image = Image.fromarray(image)
-        return image
-
-    def pose_preprocessor(self, image):
-        # Convert to numpy
-        image = np.array(image)
-        return self.controlnet_pose(image)
-
-    def scribble_preprocessor(self, image):
-        # Convert to numpy
-        image = np.array(image)
-        return self.controlnet_hed(image, scribble=True)
 
     def seg_preprocessor(self, image):
         image = image.convert('RGB')
@@ -272,46 +229,3 @@ class Predictor(BasePredictor):
         color_seg = color_seg.astype(np.uint8)
         image = Image.fromarray(color_seg)
         return image
-
-
-def ade_palette():
-    """ADE20K palette that maps each class to RGB values."""
-    return [[120, 120, 120], [180, 120, 120], [6, 230, 230], [80, 50, 50],
-            [4, 200, 3], [120, 120, 80], [140, 140, 140], [204, 5, 255],
-            [230, 230, 230], [4, 250, 7], [224, 5, 255], [235, 255, 7],
-            [150, 5, 61], [120, 120, 70], [8, 255, 51], [255, 6, 82],
-            [143, 255, 140], [204, 255, 4], [255, 51, 7], [204, 70, 3],
-            [0, 102, 200], [61, 230, 250], [255, 6, 51], [11, 102, 255],
-            [255, 7, 71], [255, 9, 224], [9, 7, 230], [220, 220, 220],
-            [255, 9, 92], [112, 9, 255], [8, 255, 214], [7, 255, 224],
-            [255, 184, 6], [10, 255, 71], [255, 41, 10], [7, 255, 255],
-            [224, 255, 8], [102, 8, 255], [255, 61, 6], [255, 194, 7],
-            [255, 122, 8], [0, 255, 20], [255, 8, 41], [255, 5, 153],
-            [6, 51, 255], [235, 12, 255], [160, 150, 20], [0, 163, 255],
-            [140, 140, 140], [250, 10, 15], [20, 255, 0], [31, 255, 0],
-            [255, 31, 0], [255, 224, 0], [153, 255, 0], [0, 0, 255],
-            [255, 71, 0], [0, 235, 255], [0, 173, 255], [31, 0, 255],
-            [11, 200, 200], [255, 82, 0], [0, 255, 245], [0, 61, 255],
-            [0, 255, 112], [0, 255, 133], [255, 0, 0], [255, 163, 0],
-            [255, 102, 0], [194, 255, 0], [0, 143, 255], [51, 255, 0],
-            [0, 82, 255], [0, 255, 41], [0, 255, 173], [10, 0, 255],
-            [173, 255, 0], [0, 255, 153], [255, 92, 0], [255, 0, 255],
-            [255, 0, 245], [255, 0, 102], [255, 173, 0], [255, 0, 20],
-            [255, 184, 184], [0, 31, 255], [0, 255, 61], [0, 71, 255],
-            [255, 0, 204], [0, 255, 194], [0, 255, 82], [0, 10, 255],
-            [0, 112, 255], [51, 0, 255], [0, 194, 255], [0, 122, 255],
-            [0, 255, 163], [255, 153, 0], [0, 255, 10], [255, 112, 0],
-            [143, 255, 0], [82, 0, 255], [163, 255, 0], [255, 235, 0],
-            [8, 184, 170], [133, 0, 255], [0, 255, 92], [184, 0, 255],
-            [255, 0, 31], [0, 184, 255], [0, 214, 255], [255, 0, 112],
-            [92, 255, 0], [0, 224, 255], [112, 224, 255], [70, 184, 160],
-            [163, 0, 255], [153, 0, 255], [71, 255, 0], [255, 0, 163],
-            [255, 204, 0], [255, 0, 143], [0, 255, 235], [133, 255, 0],
-            [255, 0, 235], [245, 0, 255], [255, 0, 122], [255, 245, 0],
-            [10, 190, 212], [214, 255, 0], [0, 204, 255], [20, 0, 255],
-            [255, 255, 0], [0, 153, 255], [0, 41, 255], [0, 255, 204],
-            [41, 0, 255], [41, 255, 0], [173, 0, 255], [0, 245, 255],
-            [71, 0, 255], [122, 0, 255], [0, 255, 184], [0, 92, 255],
-            [184, 255, 0], [0, 133, 255], [255, 214, 0], [25, 194, 194],
-            [102, 255, 0], [92, 0, 255]]
-
